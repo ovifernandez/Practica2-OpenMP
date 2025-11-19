844e0a82fe4a785e7dcdd0658e3af5e7002d0fcd
davidenrique <davidenriquea@icloud.com>
2025-11-19 12:00:40 +0100

    Parte 3 realizada con exito.

--- Practica 2 - OpenMP.cpp
+++ Practica 2 - OpenMP.cpp
@@ -6,6 +6,7 @@
 #include <cstdlib>   
 #include <string>
 #include <vector>
+#include <random>
 
 //Funcion para la Parte 1, estimacion de PI
 double funcion_x(double x) {
@@ -20,20 +21,22 @@ double funcion_x_heavy(double x) {
 
 void Parte1(long long N);
 void Parte2(long long N);
-
+void Parte3(long long N);
+void Parte3_SinFalseSharing(long long N);
 
 int main(int argc, char** argv) {
+	std::cout << "=========================================\n";
+	std::cout << "------" << "PRACTICA 2 OPENMP" << "------\n";
+	std::cout << "=========================================\n\n";
 	long long N = 10000000;
 
 	if (argc > 1) {
 		N = std::stoll(std::string(argv[1]));
 	}
-
-	std::cout << "---PRACTICA 2 OPENMP---\n" << std::endl;
-
 	Parte1(N);
 	Parte2(N);
-	//Parte3(N);
+	Parte3(N);
+	Parte3_SinFalseSharing(N);
 
 return 0;
 }
@@ -41,8 +44,9 @@ return 0;
 
 
 void Parte1(long long N) {
+	std::cout << " >>> PARTE 1.1: Calculo de PI (Secuencial)\n";
+	std::cout << " " << std::string(40, '-') << "\n";
 
-	std::cout << "(Usando maximo: " << omp_get_max_threads() << " hilos.)" << std::endl;
 	//Nos piden calcular el area debajo de la curva de f(x) desde x=0 hasta x=1.
 	//Sin embargo, f(x) no es ni un rectangulo ni un cuadrado, es una curva, por lo que para saber el area exacta, utilizamos el metodo del trapecio con subdivisiones
 			//Consiste en cortar en rebanadas la curva en N subdivisiones.
@@ -53,13 +57,11 @@ void Parte1(long long N) {
 			//3. Sumamos estas mini areas para tener el area total aproximada.
 	//Cuanto mayor sea N, mayor precisión tendremos a la curva real, ya que casda rebanada tendra una parte de arriba mas pequeña y mas parecida a una recta real.
 
-	std::cout << "-- PARTE 1.1 -- Estimacion de PI --  Version Secuencial\n" << std::endl;
 
 	double h = 1.0 / static_cast<double>(N);
 	double alturas_secuencial = 0.0;
 	double t0_sec, t1_sec, t_secuencial;
 
-	std::cout << "Calculando Secuencial con un hilo...\n" << std::endl;
 
 	t0_sec = omp_get_wtime();
 
@@ -85,17 +87,17 @@ void Parte1(long long N) {
 	t1_sec = omp_get_wtime();
 	t_secuencial = (t1_sec - t0_sec) * 1000; //En ms.
 
-	std::cout << "Secuencialmente: \n\tPI = " << std::setprecision(10) << pi_secuencial
-		<< "\n\tTiempo secuencial = " << t_secuencial << "ms\n\n" << std::endl;
+	std::cout << "    [Secuencial] PI     : " << std::fixed << std::setprecision(10) << pi_secuencial << "\n"
+		<< "    [Secuencial] Tiempo : " << std::setprecision(4) << t_secuencial << " ms\n\n";
 
 
-	std::cout << "-- PARTE 1.2 -- Estimacion de PI --  Version Paralela\n" << std::endl;
-
+	std::cout << " >>> PARTE 1.2: Calculo de PI (Paralelo)\n";
+	std::cout << " " << std::string(40, '-') << "\n";
+	std::cout << "    Calculando en paralelo...\n";
 
 	double alturas_paralelo = 0.0;
 	double t0_par, t1_par, t_paralelo;
 
-	std::cout << "Calculando Paralelamente...\n" << std::endl;
 
 	t0_par = omp_get_wtime();
 
@@ -117,15 +119,17 @@ void Parte1(long long N) {
 
 	t_paralelo = (t1_par - t0_par) * 1000; //En ms
 
-	std::cout << "\nParalelo: PI = " << std::setprecision(10) << pi_paralelo
-		<< ", Tiempo = " << t_paralelo << " ms\n" << std::endl;
+	std::cout << "    [Paralelo]   PI     : " << std::fixed << std::setprecision(10) << pi_paralelo << "\n"
+		<< "    [Paralelo]   Tiempo : " << std::setprecision(4) << t_paralelo << " ms\n";
 
 
 	// 5. Cálculo del Speedup
 	// ---
 	double speedup = t_secuencial / t_paralelo;
-	std::cout << "\n\n------------------------------------" << std::endl;
-	std::cout << "Speedup (T_seq / T_par): " << speedup << "x" << std::endl;
+	std::cout << "\n" << std::string(50, '.') << "\n";
+	std::cout << "    RESULTADO SPEEDUP (T_seq / T_par): "
+		<< "\033[1;32m" << speedup << "x" << "\033[0m" 
+		<< "\n" << std::string(50, '.') << "\n\n";
 
 	//En este caso, tras ejecutar, vemos como la funcion a ejecutar solo consta de una multiplicacion, una suma y una division, 
 	// Por lo que el tiempo secuencial es más rapido.
@@ -138,6 +142,8 @@ void Parte1(long long N) {
 
 
 void Parte2(long long N) {
+	std::cout << "\n >>> PARTE 2: Analisis de Scheduling (Carga Pesada)\n";
+	std::cout << " " << std::string(40, '-') << "\n";
 	//La función pesada es casi identica a la de la parte 1, pero añadiendo oscilaciones pequeñas, sin ser una funcion suave o predecible
 	//Esta parte trata de averiguar con que tipo de schedule conseguiremos mayor rendimiento:
 		//Static es para cuando las cargas son iguales en todos los hilos.
@@ -165,7 +171,7 @@ void Parte2(long long N) {
 	double h = 1.0 / static_cast<double>(N);
 	double t_secuencial;
 
-	std::cout << "Calculando Secuencial (1 Hilo) para f_heavy(x)..." << std::endl;
+	std::cout << "    Calculando Base Secuencial (1 Hilo)...\n";
 	//Mismo proceso que en la Parte 1 sustituyendolo por la f(x) heavy.
 	double t0_sec = omp_get_wtime();
 	double t1_sec;
@@ -178,7 +184,8 @@ void Parte2(long long N) {
 	double integral_secuencial = (h / 2.0) * alturas_secuencial;
 	t1_sec = omp_get_wtime();
 	t_secuencial = (t1_sec - t0_sec) * 1000.0; //En ms
-	std::cout << "  (Resultado Secuencial = " << std::setprecision(10) << integral_secuencial << ")" << std::endl;
+	std::cout << "    -> Tiempo Base: " << std::fixed << std::setprecision(4) << t_secuencial << " ms\n";
+	std::cout << "    -> Resultado  : " << std::setprecision(10) << integral_secuencial << "\n\n";
 
 	std::cout << "\nAnalisis de Scheduling (Tiempos en ms, Eficiencia = Speedup/hilos)" << std::endl;
 	
@@ -195,7 +202,13 @@ void Parte2(long long N) {
 		chunk = 1;
 	}
 
-	std::cout << "Hilos\tT_Static(ms)\tT_Dynamic(ms)\tT_Guided(ms)\tE_Static\tE_Dynamic\tE_Guided" << std::endl;	std::cout << "----------------------------------------------------------------------------------" << std::endl;
+	std::cout << "\nTABLA DE RENDIMIENTO (Tiempos en ms | E = Eficiencia)\n";
+	std::cout << std::string(95, '-') << "\n";
+	std::cout << std::left << std::setw(8) << "Hilos"
+		<< std::setw(12) << "T.Static" << std::setw(12) << "T.Dynamic" << std::setw(12) << "T.Guided"
+		<< " | "
+		<< std::setw(10) << "E.Static" << std::setw(10) << "E.Dynamic" << std::setw(10) << "E.Guided\n";
+	std::cout << std::string(95, '-') << "\n";
 
 	std::vector<int> hilos_a_probar = {1, 2, 4, 8, 16 }; //Con 1 hilo lo hice antes, en la version secuencial arriba.
 
@@ -254,15 +267,128 @@ void Parte2(long long N) {
 			std::cout << "Esto nunca se imprime, solo es para que el compilador no tire por tierra las variables sin hacer el bucle" << std::endl;
 		}
 
-		std::cout << p << "\t"
-			<< std::fixed << std::setprecision(4) << t_static << "\t"
-			<< std::fixed << std::setprecision(4) << t_dynamic << "\t"
-			<< std::fixed << std::setprecision(4) << t_guided << "\t"
-			<< std::fixed << std::setprecision(2) << eficiencia_static << "\t\t"
-			<< std::fixed << std::setprecision(2) << eficiencia_dynamic << "\t\t"
-			<< std::fixed << std::setprecision(2) << eficiencia_guided
-			<< std::endl;
+		std::cout << std::left << std::setw(8) << p
+			<< std::fixed << std::setprecision(2)
+			<< std::setw(12) << t_static
+			<< std::setw(12) << t_dynamic
+			<< std::setw(12) << t_guided
+			<< " | "
+			<< std::setw(10) << eficiencia_static
+			<< std::setw(10) << eficiencia_dynamic
+			<< std::setw(10) << eficiencia_guided
+			<< "\n";
 		//Hablando con los resultados en mano, podemos concluir que hay un empate tecnico entre static y guided, saliendo static como la ligera ganadora, debido a que ese desbalanceo no es tan grande como el tiempo que consume guided en gestionar la pila. Dynamic se queja un poco atrás, ya que no cuenta con la inteligencia que aplica guided, aunque la verdad es que estan los tres bastante empatados.
 
 	}
+}
+
+void Parte3(long long N) {
+	std::cout << "\n >>> PARTE 3: Montecarlo\n";
+	std::cout << " " << std::string(40, '-') << "\n";
+	//La logica es la siguiente: 
+		//Imagina un cuadrado de lados 2, Area 4 u2, centrado en el (0,0) 
+		// y dentro un circulo unitario de lado dos, que toque los bordes. Area circulo pi * r^2 = pi * 1 = pi.
+		//Lanzamos N dardos al azar, cayendo todos dentro del cuadrado, que llamaermos tablero.
+		//La proporcion de dardos que caen dentro del circulo seguirá la siguiente formula:
+		//Dardos dentro / Total lanzados = Area circulo / Area Cuadrado = pi/4
+		//Si despejamos pi = 4 x Dardos Dentro / Total lanzados. Esta será la formula que modelizaremos
+
+	//En primer lugar, generamos un bucle for que genera una X y una Y en cada iteracion (dardo), ambos entre (-1,1).
+	//Después, comrpobamos si es un Dardo Dentro, usando pitágoras. x^2 + y^2 <= 1
+	// Si la distancia al centro (0,0) es menor o igual que 1, quiere decirnos que estamos dentro del circulo.
+	
+	//Si el dardo cae dentro, hay que sumar el contador +1. Sin embargo, al estar todo en paralelo, esta variable hay que protegerla, por lo que podriamos aplicar reduction, con sus acumuladores privados por hilo, para evitar peleas entre hilos al querer modificar el acumulador.
+
+	long long aciertos = 0;
+	double t0 = omp_get_wtime();
+
+#pragma omp parallel reduction(+: aciertos)
+	{
+		//La semilla se basa en el numero de hilo, su id
+	unsigned int seed = omp_get_thread_num();
+	//Cada hilo crea su propio generador, para eliminar carreras posibles, mediante la semilla más el tiempo de creacion del hilo. 
+	//La distribucion es uniforme, de -1 a 1.
+	std::mt19937 generador(seed + (unsigned int)time(NULL));
+	std::uniform_real_distribution<double> distribucion(-1.0, 1.0);
+
+	#pragma omp for schedule(static)
+		for (long long i = 0; i < N; i++) {
+			double x = distribucion(generador);
+			double y = distribucion(generador);
+			if ((x*x + y*y) <= 1) {
+				aciertos++;
+			}
+		}
+	}
+	double t1 = omp_get_wtime();
+	double tiempo_montecarlo = (t1 - t0) * 1000.0; // Pasamos a milisegundos
+
+	double pi;
+	pi = 4.0 * static_cast<double>(aciertos) / static_cast<double>(N);
+
+	std::cout << "    [MonteCarlo] PI Estimado : " << std::fixed << std::setprecision(10) << pi << "\n";
+	std::cout << "    [MonteCarlo] Tiempo      : " << std::setprecision(4) << tiempo_montecarlo << " ms\n";
+	//En esta paralelizacion, la granularidad es muy gruesa.
+	// Esto quiere decir que, si medimos el tiempo que se tarda en generar un numero aleatorio de calidad es el siguiente:
+	//Sin embargo, en la Parte 1 se tarda infinitamente menos en cada iteracion del bucle de reduccion. 
+	//Hablando de la sincronizacion, que preferimos? Una granularidad fina, que se vea eclipsada por el tiempo de comunicacion (overhead), o una gruesa, para que el tiempo de overhead sea insignificante?.
+	//Por ello, el tiempo de sincronizacion se nota muchisimo más cuando estamos tratando con operaciones muy simples, que cuando tratamos con operaciones grandes.
+}
+
+struct alignas(64) Contador {
+	long long valor;
+};
+
+void Parte3_SinFalseSharing(long long N) {
+	//Para el apartado de optimizar este ejercicio, hemos decidido explorar el de False Sharing.
+	//A veces se produce este false sharing del que hablamos.
+	//Si dos hilos (a y b) tienen sus propios contadores privados (contA, contB) y da la casualidad que ambos contadores se almacenen contiguos en memoria RAM,
+	//, ambos contadores viajaran en el mismo bloque, lo que obliga a B esperar a que A termine o viceversa, ya que no pueden usar el mismo bloque a la vez.
+	//Porque usan el mismo bloque? Pues pq la RAM y la CPU se comunican por lineas de cache de 64B, porque la CPU no lee variables (long long ocupa 8B), lee bloques.
+	//Por ello, si necesito tocar contadorA, me traigo el bloque en donde vive ese contador en RAM, y inevitablemente tb me traigo el contadorB que vive al lado suyo en el mismo bloque.
+	//Si B quisiese escribir en su contador, se le pararian los pies, ya que ese bloque estaria ocupado temporalmente.
+
+
+	//Como solucionamos esto? Como evitamos que dos contadores se hallen en el mismo bloque? Muy sencillo!
+	//Hacemos que cada contador tenga una estructura que ocupe 64B, aunque de esos 64B solo use los 8B del valor de long long contador.
+	//De esta manera, te aseguras al 100% que no se van a producir más condiciones de false sharing.
+
+	std::cout << "\n >>> PARTE 3.2: Monte Carlo (Optimizado - Padding)\n";
+	std::cout << " " << std::string(40, '-') << "\n";
+	//Esto otorga a cada hilo su propio contador privado, que ocupa 64B.
+	
+	int numHilos = omp_get_max_threads();
+	std::vector<Contador> contadores(numHilos);
+	for (auto& c : contadores) c.valor = 0;
+
+	double t0 = omp_get_wtime();
+#pragma omp parallel
+	{
+		int id = omp_get_thread_num();
+
+		unsigned int seed = (unsigned int)time(NULL) + id;
+		std::mt19937 generador(seed);
+		std::uniform_real_distribution<double> distrib(-1.0, 1.0);
+
+#pragma omp for schedule(static)
+		for (long long i = 0; i < N; i++) {
+			double x = distrib(generador);
+			double y = distrib(generador);
+
+			if (x * x + y * y <= 1.0) {
+				contadores[id].valor++;  // Sin false sharing
+			}
+		}
+	}
+
+	long long totalAciertos = 0;
+	for (auto& c : contadores) totalAciertos += c.valor;
+
+	double t1 = omp_get_wtime();
+
+	double pi = 4.0 * (double)totalAciertos / (double)N;
+
+	std::cout << "    [Optimizado] PI Estimado : " << std::fixed << std::setprecision(10) << pi << "\n";
+	std::cout << "    [Optimizado] Tiempo      : " << std::setprecision(4) << (t1 - t0) * 1000.0 << " ms\n\n";
+
 }
